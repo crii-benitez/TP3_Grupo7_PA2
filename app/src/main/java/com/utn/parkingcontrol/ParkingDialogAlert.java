ff661ef64bf37b0b809c912e3e902c8edf37693d
sbarzola <sbarzola@arsat.com.ar>
2022-10-02 01:43:43 -0300

    intento base de datos parking

--- app/src/main/java/com/utn/parkingcontrol/ParkingDialogAlert.java
+++ app/src/main/java/com/utn/parkingcontrol/ParkingDialogAlert.java
@@ -3,25 +3,29 @@ package com.utn.parkingcontrol;
 import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ParkingDialogAlert  {
 
-    public static AlertDialog CreateAlertDialog(Context context, LayoutInflater inflater) {
+    public static AlertDialog CreateAlertDialog(Context context, LayoutInflater inflater,User user) {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
 
         View view = inflater.inflate(R.layout.register_parking, null);
 
+        TextView tvUserName = (TextView) view.findViewById(R.id.txtUserName);
         EditText et_Matricula = (EditText) view.findViewById(R.id.txtNroMatricula);
         EditText et_Tiempo = (EditText) view.findViewById(R.id.txtTiempo);
         Button cancelar = view.findViewById(R.id.btnCancelar);
         Button registrar = view.findViewById(R.id.btnRegistrar);
+        tvUserName.setText(user.getName());
 
         builder.setView(view);
 
@@ -37,7 +41,7 @@ public class ParkingDialogAlert  {
         registrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(v.getContext(), "administracion", null, 1);
+                AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(context, "administracion", null, 1);
                 SQLiteDatabase db = admin.getWritableDatabase();
 
                 String matricula = et_Matricula.getText().toString();
@@ -45,15 +49,23 @@ public class ParkingDialogAlert  {
 
                 if (!matricula.isEmpty() && !tiempo.isEmpty()) {
                     ContentValues registro = new ContentValues();
-                    registro.put("matricula", matricula);
-                    registro.put("tiempo", tiempo);
+                    registro.put("name", matricula);
+                    registro.put("email", tiempo);
+                    registro.put("pass", user.getName());
 
-                    db.insert("parkings", null, registro);
+                    try{
+                        db.insert("users2", null, registro);
 
-                    Toast.makeText(v.getContext(), "Datos guardados", Toast.LENGTH_SHORT).show();
-                    alertDialog.dismiss();
+                        Toast.makeText(v.getContext(), "Datos guardados", Toast.LENGTH_SHORT).show();
+                        alertDialog.dismiss();
+                    }
+                    catch (Exception e) {
+                        Toast.makeText(v.getContext(), "error:" + e , Toast.LENGTH_SHORT).show();
+
+                    }
+
                 } else {
-                    Toast.makeText(v.getContext(), "Debes llenar Matricula y Tiempo", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(context, "Debes llenar Matricula y Tiempo", Toast.LENGTH_SHORT).show();
                 }
                 db.close();
             }
